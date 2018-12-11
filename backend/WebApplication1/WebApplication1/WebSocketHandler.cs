e124e7d4a9d7785e792e1d2cf6354538e81185f2
juhana.poikela <juhana.poikela@gmail.com>
2018-12-11 08:28:20 +0000

    Added WebSocketHandler

---
+++ backend/WebApplication1/WebApplication1/WebSocketHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Web.WebSockets;
+using WebApplication1.ApiModels;
+
+namespace WebApplication1
+{
+    public class ServerEventHandler : WebSocketHandler
+    {
+        public override void OnOpen()
+        {
+            base.Send("You connected to a WebSocket!");
+            GameObject gameObject = new GameObject();
+            gameObject.runGame();
+        }
+
+        public override void OnMessage(string message)
+        {
+            // Echo message
+            base.OnMessage(message);
+        }
+
+        public override void OnClose()
+        {
+            // Free resources, close connections, etc.
+            base.OnClose();
+        }
+    }
+}
