14391cb54445189e85f1aa43f0b8ad0144bc3c08
Markus Heino <markus.heino92@gmail.com>
2018-12-11 12:21:22 +0200

    Front forward

--- frontend/app.js
+++ frontend/app.js
@@ -27,6 +27,9 @@ function sendStandRequest() {
 function joinGame() {
     console.log("Joining game!");
 }
+function quitGame() {
+    console.log("Quite Game!")
+}
 //getting card from available deck that is stored in the server
 function httpGetCard(theUrl)
 {
@@ -40,6 +43,7 @@ function getCardImg (name) {
    
     var name = "d5";
     var card = 5;
+
     if (card == 1) {
         console.log("card oli 1");
         fullname = os.path.join("cards/", name)
@@ -53,22 +57,95 @@ function getCardImg (name) {
     }
 }
 function getPlayerNumber() {
-    if (PlayerId == 1) {
 
+    PlayerId = 0;
+    numberofcards = 2;
+    if (PlayerId == 0) {
+        this.document.getElementById("playercard").className = "dealercard";
+        if (numberofcards == 2) {
+            document.getElementById("playercard").style.left="460"
+
+        }
+        else if (numberofcards == 3) {
+            document.getElementById("playercard").style.left="470"
+
+        }
+        else if (numberofcards == 4) {
+            document.getElementById("playercard").style.left="480"
+
+        }
+        else if (numberofcards == 5) {
+            document.getElementById("playercard").style.left="490"
+
+        }
+    }
+    else if (PlayerId == 1) {
+    this.document.getElementById("playercard").className = "playeronecard";
+        if (numberofcards == 2) {
+            document.getElementById("playercard").style.left="200"
+
+        }
+        else if (numberofcards == 3) {
+            document.getElementById("playercard").style.left="210"
+
+        }
+        else if (numberofcards == 4) {
+            document.getElementById("playercard").style.left="220"
+
+        }
+        else if (numberofcards == 5) {
+            document.getElementById("playercard").style.left="230"
+
+        }
     }
     else if (PlayerId == 2) {
+        this.document.getElementById("playercard").className = "playertwocard";
+        if (numberofcards == 2) {
+            document.getElementById("playercard").style.left="410"
+
+        }
+        else if (numberofcards == 3) {
+            document.getElementById("playercard").style.left="420"
+
+        }
+        else if (numberofcards == 4) {
+            document.getElementById("playercard").style.left="430"
+
+        }
+        else if (numberofcards == 5) {
+            document.getElementById("playercard").style.left="440"
+
+        }
         
     }
     else if (PlayerId == 3) {
-        
-    }
-    else if (PlayerId == 4) {
-        
+        this.document.getElementById("playercard").className = "playerthreecard";
+        if (numberofcards == 2) {
+            document.getElementById("playercard").style.left="645"
+
+        }
+        else if (numberofcards == 3) {
+            document.getElementById("playercard").style.left="655"
+
+        }
+        else if (numberofcards == 4) {
+            document.getElementById("playercard").style.left="665"
+
+        }
+        else if (numberofcards == 5) {
+            document.getElementById("playercard").style.left="675"
+
+        }
     }
     else {
-        document.getElementById("playercard").className = "playeronecard";
+        console.log("errori")
+        
     }
+    
+    
+    
 }
+
 
 
 
